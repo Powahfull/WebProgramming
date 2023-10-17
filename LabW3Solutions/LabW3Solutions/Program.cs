04e51086720066aa141536b7ad306714a07a607a
Kıvanç Karahasanoğlu <kivanc.w8@hotmail.com>
2023-10-17 21:47:43 +0300

    added LabW3Solutions
    runtime on Windows

---
+++ LabW3Solutions/LabW3Solutions/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        static void main(string[] args)
+        {
+
+            Console.WriteLine(IfYearIsLeap(2020));
+
+            static bool IfYearIsLeap(int year)
+            {
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+        }
+    }
+}
