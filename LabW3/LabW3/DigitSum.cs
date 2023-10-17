31a3a8cfc5c2d18441e616b9d0c90c3714b2d442
Kıvanç <kivanc.w8@hotmail.com>
2023-10-17 15:19:16 +0300

    update on Mac
    runtime

---
+++ LabW3/LabW3/DigitSum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LabW3
+{
+	public class DigitSum
+	{
+		public static int DigitS(int a)
+		{
+			int total = 0;
+			while (a >0)
+			{
+				total += a % 10;
+				a = a / 10;
+			}
+            return total;
+        }
+	}
+}
+
+
---
+++ LabW3/LabW3/People.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace LabW3
+{
+
+    internal class People
+    {
+        static void main(String[] args)
+        {
+            Person person = new Person();
+            person.greet();
+        }
+    }
+
+    }
+    class Person
+    {
+        public int Age { get; set; }
+
+        public void greet()
+        {
+
+        }
+    }
+
+    class Student: Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I'm studying");
+        }
+
+        public void showAge()
+        {
+            Console.WriteLine("My age is: " + Age);
+        }
+    }
+
+    class Teacher
+    {
+        public void explain()
+        {
+            Console.WriteLine("Explain");
+        }
+    }
+
+
+
